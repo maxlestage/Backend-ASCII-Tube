5819b301936b2e06231dcb210bf0d824f3cf6731
maxlestage <maxlestage@icloud.com>
2023-03-29 09:13:03 +0200

    upload video : clean

--- api/src/upload.rs
+++ api/src/upload.rs
@@ -9,10 +9,8 @@ use salvo::{
 #[handler]
 pub async fn upload(req: &mut Request, res: &mut Response) {
     let file = req.file("").await;
-    println!("{:#?}", file);
     if let Some(file) = file {
         let dest = format!("temp/{}", file.name().unwrap_or("file"));
-        println!("{}", dest);
         let info = if let Err(e) = std::fs::copy(&file.path(), Path::new(&dest)) {
             res.set_status_code(StatusCode::INTERNAL_SERVER_ERROR);
             format!("file not found in request: {}", e)
