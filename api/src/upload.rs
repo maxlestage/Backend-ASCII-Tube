2807fc813a118a2286f7ff93474fd06a90e5fef5
maxlestage <maxlestage@icloud.com>
2023-04-14 19:09:07 +0200

    ok j'ai laché une perle

--- api/src/upload.rs
+++ api/src/upload.rs
@@ -7,15 +7,8 @@ use salvo::{
 };
 
 pub async fn upload(req: &mut Request, res: &mut Response) {
-    // select_user_by_email(jwt) -> {id mail etc ....}
-
-    let file = req.first_file().await; // chrislearn — 29/03/2023 à 09:18 use req.first_file().await to get the uploaded file instead of req.file("").await
-    if let Some(file) = file {
+    if let Some(file) = req.first_file().await {
         let dest = format!("temp/{}", file.name().unwrap_or("file"));
-        let info = if let Err(e) = std::fs::copy(&file.path(), Path::new(&dest)) {
-            dest
-        } else {
-            dest
-        };
+        let _info: Result<u64, std::io::Error> = std::fs::copy(&file.path(), Path::new(&dest));
     }
 }
