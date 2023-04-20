1fa9545d8f213a233bce9e255c84c61427d213f2
zHatsuharu <zhatsuharu@gmail.com>
2023-04-20 22:45:42 +0200

    Add CORS (only on register ROUTE)

--- api/src/server.rs
+++ api/src/server.rs
@@ -5,7 +5,7 @@ use auth::jwt_auth::{sign_in, JwtClaims, SECRET_KEY};
 use db::db_connection::db_connection;
 use migration::{Migrator, MigratorTrait};
 use salvo::jwt_auth::HeaderFinder;
-use salvo::{__private::tracing, prelude::*};
+use salvo::{__private::tracing, prelude::*, cors::Cors};
 use sea_orm::DatabaseConnection;
 
 pub async fn main() {
@@ -17,10 +17,13 @@ pub async fn main() {
         .with_finders(vec![Box::new(HeaderFinder::new())])
         .with_response_error(true);
 
+    let cors_handler = Cors::builder()
+        .allow_any_origin().allow_methods(vec!["GET", "POST", "DELETE"]).build();
+
     let router = Router::new()
         .get(hello_world)
         .push(Router::with_path("video/upload").post(upload_video))
-        .push(Router::with_path("signup").post(sign_up))
+        .push(Router::with_path("signup").hoop(cors_handler).post(sign_up))
         .push(Router::with_path("login").post(sign_in))
         .push(Router::with_path("video/<id>").get(get_video))
         .push(Router::with_path("video/<id>").delete(delete_video))
