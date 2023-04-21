1301bd2c37a24f2043ce737e9856c20f11a96af0
Donoma2 <dorianlevee@gmail.com>
2023-04-22 01:06:43 +0200

    add '/api' before every routes to avoid conflict with front

--- api/src/server.rs
+++ api/src/server.rs
@@ -6,7 +6,7 @@ use auth::jwt_auth::{sign_in, JwtClaims, SECRET_KEY};
 use db::db_connection::db_connection;
 use migration::{Migrator, MigratorTrait};
 use salvo::jwt_auth::HeaderFinder;
-use salvo::{__private::tracing, cors::Cors, prelude::*};
+use salvo::{__private::tracing, prelude::*};
 use sea_orm::DatabaseConnection;
 
 pub async fn main() {
@@ -18,21 +18,16 @@ pub async fn main() {
         .with_finders(vec![Box::new(HeaderFinder::new())])
         .with_response_error(true);
 
-    let cors_handler = Cors::builder()
-        .allow_any_origin()
-        .allow_methods(vec!["GET", "POST", "DELETE"])
-        .build();
-
     let router = Router::new()
         .get(hello_world)
-        .push(Router::with_path("video/upload").post(upload_video))
-        .push(Router::with_path("signup").hoop(cors_handler).post(sign_up))
-        .push(Router::with_path("login").post(sign_in))
-        .push(Router::with_path("video/<id>").get(get_video))
-        .push(Router::with_path("video/<id>").delete(delete_video))
-        .push(Router::with_path("video/<video_id>/<user_id>").post(create_comment))
-        .push(Router::with_path("video/<video_id>/comment/").get(get_comment))
-        .push(Router::with_path("video/<video_id>/comment/<id>").delete(delete_comment))
+        .push(Router::with_path("api/video/upload").post(upload_video))
+        .push(Router::with_path("api/signup").post(sign_up))
+        .push(Router::with_path("api/login").post(sign_in))
+        .push(Router::with_path("api/video/<id>").get(get_video))
+        .push(Router::with_path("api/video/<id>").delete(delete_video))
+        .push(Router::with_path("api/video/<video_id>/<user_id>").post(create_comment))
+        .push(Router::with_path("api/video/<video_id>/comment/").get(get_comment))
+        .push(Router::with_path("api/video/<video_id>/comment/<id>").delete(delete_comment))
 
         // .push(
         //     Router::with_path("upload")
